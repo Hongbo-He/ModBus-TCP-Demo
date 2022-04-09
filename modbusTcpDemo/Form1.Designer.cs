310e14af210be89a5ad6130d539c1d5531717ebd
Hongbo_He <2834568564@qq.com>
2022-04-09 23:44:55 +0800

    添加项目文件。

---
+++ modbusTcpDemo/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace modbusTcpDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.连接 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // 连接
+            // 
+            this.连接.Location = new System.Drawing.Point(456, 194);
+            this.连接.Name = "连接";
+            this.连接.Size = new System.Drawing.Size(75, 23);
+            this.连接.TabIndex = 0;
+            this.连接.Text = "button1";
+            this.连接.UseVisualStyleBackColor = true;
+            this.连接.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(67, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(669, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "发送";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(67, 232);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(669, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "接收";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.连接);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button 连接;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ modbusTcpDemo/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace modbusTcpDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        Boolean flag = true;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con();
+            timer1.Enabled = true;
+
+
+        }
+
+        private Socket clientSocket;
+
+        public void con() {
+            int port = Convert.ToInt32("502");
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try {
+                clientSocket.Connect(endPoint);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+
+        public void recvSth() {
+            while (true)
+            {
+                byte[] data = new byte[1024];
+
+                int len = clientSocket.Receive(data);
+                while (len == 0) {
+                    continue;
+                }
+                int dataSize = data[8];
+
+                byte[] data2 = new byte[dataSize];
+                Console.WriteLine(dataSize);
+                for (int i = 0; i < dataSize; i++) {
+                    data2[i] = data[i + 9];
+                }
+                String re = BitConverter.ToString(data2);
+                //label2.Text = re + "/r/n" + DateTime.Now;
+                updateUI2(re + "  时间" + DateTime.Now);
+                Console.WriteLine(re);
+
+
+            }
+
+        }
+        delegate void changeText(String msg);
+
+        private void delegateChangeLB2(String msg) {
+            label2.Text = msg;
+        }
+
+        private void delegateChangeLB1(String msg)
+        {
+            label1.Text = msg;
+        }
+
+
+        private void updateUI(String msg) {
+            changeText change = new changeText(delegateChangeLB1);
+            this.groupBox1.Invoke(change,msg);
+        }
+        private void updateUI2(String msg)
+        {
+            changeText change = new changeText(delegateChangeLB2);
+            this.groupBox1.Invoke(change, msg);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            timer1.Interval = 1000;
+            if (clientSocket.Connected)
+            {
+                byte[] data = new byte[] { 0x01, 0x00, 0x00, 0x00, 0x00, 0x06, 0x01, 0x03, 0x00, 0x00, 0x00, 0x0A };
+                //label1.Text = BitConverter.ToString(data);
+                updateUI(BitConverter.ToString(data));
+                clientSocket.Send(data);
+                Console.WriteLine("发送完毕");
+            }
+            else
+            {
+                Console.WriteLine("未连接");
+                flag = true;
+            }
+            if (flag)
+            {
+                ThreadStart threadStart = new ThreadStart(recvSth);
+                Thread thread = new Thread(threadStart);
+                thread.IsBackground = true;
+                thread.Start();
+
+                flag = false;
+            }
+        }
+    }
+}
